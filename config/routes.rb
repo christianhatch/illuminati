1614522997876dadc5f197345f84e2f3de9c0e2c
Christian Hatch <christianhatch@gmail.com>
2017-03-13 21:17:42 +0000

    added profiles controller, new view, and link on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   root to: 'pages#home'
   devise_for :users, controllers: { registrations: 'users/registrations' }
+ 
   resources :users do
     resource :profile
   end
