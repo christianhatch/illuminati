8e841b0691defb3bd2b592ad12cf2b788dfb4dd3
Christian Hatch <christianhatch@gmail.com>
2017-03-14 01:21:25 +0000

    added user profile index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
    
    before_action :authenticate_user!
 
+   # GET to /users
+   def index
+      @users = User.includes(:profile)
+   end
 
    # GET to /users/:id
    def show
